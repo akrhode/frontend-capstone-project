d61f4c6300f641e31548ecdf7f1d2f3d90536f6d
akrhode <a.rhode@web.de>
2024-01-05 00:38:05 +0100

    Add meta tags and favicons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Homepage</h1>
+      </div>
+    </>
   );
 }
 
