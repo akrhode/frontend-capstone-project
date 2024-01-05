93059db2b6e551c4b03f410c7c39a02cbd2456f2
akrhode <a.rhode@web.de>
2024-01-05 15:44:42 +0100

    Add routes, build responsive navbar, add styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Main from "./components/layout/Main";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Home from "./pages/Home";
+import Reservations from "./pages/Reservations.js";
 
 const App = () => {
   return (
     <>
-      <Main></Main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Main = ({ children }) => {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Main;
